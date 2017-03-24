a7cccb324f50b7466d8cc506dd0d408e3d29d584
ipoeadmin <antonio.fck@gmail.com>
2017-03-24 09:20:30 +0400

    modified:   radius.h

--- radius.h
+++ radius.h
@@ -8,6 +8,14 @@
 #define RAD_ACCOUNTING_REQUEST 	4
 #define RAD_ACCOUNTING_RESPONSE	5
 
+/* Limits */
+#define ERRSIZE         128             /* Maximum error message length */
+#define MAXCONFLINE     1024            /* Maximum config file line length */
+#define MAXSERVERS      10              /* Maximum number of servers to try */
+#define MSGSIZE         4096            /* Maximum RADIUS message */
+#define PASSSIZE        128             /* Maximum significant password chars */
+
+
 extern int radius_authenticate(struct authdata_t *);
 extern void radius_close(struct  authdata_t *);
 
